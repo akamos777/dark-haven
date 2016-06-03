9fd4f0487272a0ffe3e66eb71e3fc1d69feb6a5a
akamos777 <akamos777@gmail.com>
2016-06-02 23:42:37 -0400

    First commit for darkhaven game development

---
+++ android/src/com/tesseractgamingindustries/darkhaven/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tesseractgamingindustries.darkhaven;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tesseractgamingindustries.darkhaven.DarkHaven;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DarkHaven(), config);
+	}
+}
